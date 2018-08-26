ce81b600c4956385a7fbe497f3086223323f1ca6
shrutika2402 <shrutika.vgupta@gmail.com>
2018-08-26 08:26:25 -0400

    no comments

---
+++ MySampleTest/src/TestPackage/MySampleTest.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class MySampleTest {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
